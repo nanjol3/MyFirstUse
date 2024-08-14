e9d77ee4ea3e09f39262a19fdd4f0e6643d429b8
nanjol <86716763+nanjol3@users.noreply.github.com>
2024-08-14 16:37:57 +0800

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,66 +1,124 @@
-import pandas as pd
-import re
+-- transfer_permissions.sql
 
-def extract_table_names(query):
-    # 将查询转换为小写以简化处理
-    query = query.lower()
+-- 创建jtdt用户（如果不存在）
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'jtdt') THEN
+    CREATE USER jtdt WITH PASSWORD 'secure_password';
+  END IF;
+END
+$$;
+
+-- 复制m910b的角色属性
+DO $$
+DECLARE
+    m910b_attrs record;
+BEGIN
+    SELECT * INTO m910b_attrs FROM pg_authid WHERE rolname = 'm910b';
     
-    # 复杂的正则表达式模式
-    patterns = [
-        # FROM, JOIN 子句
-        r'(?:from|join)\s+(\w+(?:\.\w+)?(?:\s+(?:as\s+)?\w+)?)',
-        # INSERT INTO, UPDATE, DELETE FROM 语句
-        r'(?:insert\s+into|update|delete\s+from)\s+(\w+(?:\.\w+)?)',
-        # CREATE, ALTER, DROP TABLE 语句
-        r'(?:create|alter|drop)\s+table\s+(\w+(?:\.\w+)?)',
-        # TRUNCATE 语句
-        r'truncate\s+(?:table\s+)?(\w+(?:\.\w+)?)',
-        # VACUUM, ANALYZE, CLUSTER, REINDEX 语句 (PostgreSQL 特定)
-        r'(?:vacuum\s+(?:full\s+)?|analyze\s+|cluster\s+|reindex\s+table\s+)(\w+(?:\.\w+)?)',
-        # WITH 子句 (CTE)
-        r'with\s+(\w+(?:\.\w+)?)\s+(?:as\s+)?\(',
-        # table(column) 形式, 通常在INSERT语句中使用
-        r'(\w+(?:\.\w+)?)\s*\([^\)]*\)',
-    ]
-    
-    tables = set()
-    for pattern in patterns:
-        matches = re.findall(pattern, query)
-        for match in matches:
-            # 清理表名（去除可能的别名和额外的空格）
-            table = re.split(r'\s+', match.strip())[0]
-            # 移除可能的引号
-            table = table.strip('"\'`')
-            tables.add(table)
-    
-    return list(tables)
+    EXECUTE format('ALTER USER jtdt WITH %s %s %s %s %s %s %s %s',
+        CASE WHEN m910b_attrs.rolsuper THEN 'SUPERUSER' ELSE 'NOSUPERUSER' END,
+        CASE WHEN m910b_attrs.rolinherit THEN 'INHERIT' ELSE 'NOINHERIT' END,
+        CASE WHEN m910b_attrs.rolcreaterole THEN 'CREATEROLE' ELSE 'NOCREATEROLE' END,
+        CASE WHEN m910b_attrs.rolcreatedb THEN 'CREATEDB' ELSE 'NOCREATEDB' END,
+        CASE WHEN m910b_attrs.rolcanlogin THEN 'LOGIN' ELSE 'NOLOGIN' END,
+        CASE WHEN m910b_attrs.rolreplication THEN 'REPLICATION' ELSE 'NOREPLICATION' END,
+        CASE WHEN m910b_attrs.rolbypassrls THEN 'BYPASSRLS' ELSE 'NOBYPASSRLS' END,
+        CASE WHEN m910b_attrs.rolconnlimit >= 0 THEN 'CONNECTION LIMIT ' || m910b_attrs.rolconnlimit::text ELSE '' END
+    );
+END $$;
 
-# 假设你的DataFrame名为df，并且有一个'query'列
-data = {
-    'query': [
-        'SELECT * FROM table1 JOIN table2 ON table1.id = table2.id',
-        'INSERT INTO table3 (col1, col2) SELECT * FROM table4',
-        'WITH cte AS (SELECT * FROM table5) SELECT * FROM cte JOIN table6',
-        'UPDATE table7 SET col = val WHERE id IN (SELECT id FROM table8)',
-        'VACUUM FULL table9',
-        'ANALYZE table10',
-        'CLUSTER table11 USING index_name',
-        'REINDEX TABLE table12',
-        'CREATE TABLE new_table AS SELECT * FROM old_table',
-        'ALTER TABLE table13 ADD COLUMN new_col INT',
-        'DROP TABLE IF EXISTS table14',
-        'TRUNCATE table15',
-        'DELETE FROM table16 WHERE condition',
-        'WITH RECURSIVE cte (n) AS (SELECT 1 UNION ALL SELECT n + 1 FROM cte WHERE n < 5) SELECT * FROM cte'
-    ]
-}
-df = pd.DataFrame(data)
+-- 复制数据库级别的权限
+DO $$
+DECLARE
+    db record;
+BEGIN
+    FOR db IN SELECT datname FROM pg_database WHERE datname NOT IN ('template0', 'template1', 'postgres')
+    LOOP
+        EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE %I TO jtdt', db.datname);
+    END LOOP;
+END $$;
 
-# 应用函数到DataFrame
-df['tables'] = df['query'].apply(extract_table_names)
+-- 复制模式级别和对象级别的权限
+DO $$
+DECLARE
+    schema_name text;
+    obj record;
+BEGIN
+    FOR schema_name IN SELECT nspname FROM pg_namespace WHERE nspname NOT LIKE 'pg_%' AND nspname != 'information_schema'
+    LOOP
+        -- 授予模式使用权限
+        EXECUTE format('GRANT USAGE ON SCHEMA %I TO jtdt', schema_name);
+        EXECUTE format('GRANT ALL PRIVILEGES ON SCHEMA %I TO jtdt', schema_name);
+        
+        -- 授予模式中所有表的权限
+        EXECUTE format('GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA %I TO jtdt', schema_name);
+        EXECUTE format('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA %I TO jtdt', schema_name);
+        EXECUTE format('GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA %I TO jtdt', schema_name);
+        
+        -- 设置未来对象的默认权限
+        EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA %I GRANT ALL PRIVILEGES ON TABLES TO jtdt', schema_name);
+        EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA %I GRANT ALL PRIVILEGES ON SEQUENCES TO jtdt', schema_name);
+        EXECUTE format('ALTER DEFAULT PRIVILEGES IN SCHEMA %I GRANT ALL PRIVILEGES ON FUNCTIONS TO jtdt', schema_name);
+    END LOOP;
+END $$;
 
-# 显示结果
-print(df)
+-- 将jtdt添加到m910b所属的所有角色中
+DO $$
+DECLARE
+    role_name text;
+BEGIN
+    FOR role_name IN SELECT r.rolname
+                     FROM pg_auth_members m
+                     JOIN pg_roles r ON (m.roleid = r.oid)
+                     WHERE m.member = (SELECT oid FROM pg_roles WHERE rolname = 'm910b')
+    LOOP
+        EXECUTE format('GRANT %I TO jtdt', role_name);
+    END LOOP;
+END $$;
 
-# 如果你想要将列表转换为字符串，可以使用：
-# df['tables'] = df['tables'].apply(lambda x: ', '.join(x) if x else '')
+-- 复制表的所有权（如果需要）
+DO $$
+DECLARE
+    obj record;
+BEGIN
+    FOR obj IN SELECT schemaname, tablename 
+               FROM pg_tables 
+               WHERE tableowner = 'm910b'
+    LOOP
+        EXECUTE format('ALTER TABLE %I.%I OWNER TO jtdt', obj.schemaname, obj.tablename);
+    END LOOP;
+END $$;
+
+-- 复制序列的所有权（如果需要）
+DO $$
+DECLARE
+    obj record;
+BEGIN
+    FOR obj IN SELECT schemaname, sequencename 
+               FROM pg_sequences 
+               WHERE sequenceowner = 'm910b'
+    LOOP
+        EXECUTE format('ALTER SEQUENCE %I.%I OWNER TO jtdt', obj.schemaname, obj.sequencename);
+    END LOOP;
+END $$;
+
+-- 复制函数的所有权（如果需要）
+DO $$
+DECLARE
+    obj record;
+BEGIN
+    FOR obj IN SELECT n.nspname as schema_name, p.proname as function_name, pg_get_function_identity_arguments(p.oid) as args
+               FROM pg_proc p
+               JOIN pg_namespace n ON p.pronamespace = n.oid
+               WHERE p.proowner = (SELECT oid FROM pg_roles WHERE rolname = 'm910b')
+    LOOP
+        EXECUTE format('ALTER FUNCTION %I.%I(%s) OWNER TO jtdt', obj.schema_name, obj.function_name, obj.args);
+    END LOOP;
+END $$;
+
+-- 提示完成
+DO $$
+BEGIN
+    RAISE NOTICE 'Permission transfer from m910b to jtdt completed.';
+END $$;
