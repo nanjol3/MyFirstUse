c44d4a8d784de0ddb6aa8f1369c3d43271b82ada
nanjol <86716763+nanjol3@users.noreply.github.com>
2024-08-15 12:31:21 +0800

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,23 +1,70 @@
--- 授予当前数据库中所有现有schema的权限
-GRANT ALL PRIVILEGES ON ALL SCHEMAS IN DATABASE CURRENT_DATABASE() TO jtdt;
+import psycopg2
+from psycopg2 import sql
 
--- 授予当前数据库中所有现有表的权限
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO jtdt;
+def connect_to_remote_db(host, database, user, password, port):
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password,
+            port=port
+        )
+        print("成功连接到远程数据库")
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print("连接到远程数据库时出错:", error)
+        return None
 
--- 为未来创建的对象设置默认权限
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT ALL PRIVILEGES ON TABLES TO jtdt;
+def execute_query(connection, query, params=None):
+    try:
+        with connection.cursor() as cursor:
+            if params:
+                cursor.execute(query, params)
+            else:
+                cursor.execute(query)
+            
+            if cursor.description:
+                return cursor.fetchall()
+            else:
+                connection.commit()
+                return None
+    except (Exception, psycopg2.Error) as error:
+        print("执行查询时出错:", error)
+        return None
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT ALL PRIVILEGES ON SEQUENCES TO jtdt;
+# 数据库连接配置
+db_config = {
+    "host": "52.190.93.13",
+    "port": 41011,
+    "database": "mergeinto",
+    "user": "jtdt",
+    "password": "Gauss_234"
+}
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA public
-GRANT ALL PRIVILEGES ON FUNCTIONS TO jtdt;
-
--- 如果有其他schema，也需要为它们设置默认权限
--- 替换 'other_schema' 为实际的schema名称
--- ALTER DEFAULT PRIVILEGES IN SCHEMA other_schema
--- GRANT ALL PRIVILEGES ON TABLES TO jtdt;
-
--- 确保jtdt用户有连接到该数据库的权限
-GRANT CONNECT ON DATABASE CURRENT_DATABASE() TO jtdt;
+# 主程序
+if __name__ == "__main__":
+    # 连接到数据库
+    conn = connect_to_remote_db(**db_config)
+    
+    if conn:
+        try:
+            # 执行示例查询，从mergeinto schema中选择一个表
+            query = sql.SQL("SELECT * FROM {}.your_table LIMIT 5;").format(sql.Identifier('mergeinto'))
+            results = execute_query(conn, query)
+            
+            if results:
+                for row in results:
+                    print(row)
+            else:
+                print("查询没有返回任何结果。")
+            
+        except (Exception, psycopg2.Error) as error:
+            print("执行查询时出错:", error)
+        
+        finally:
+            # 关闭数据库连接
+            conn.close()
+            print("数据库连接已关闭")
+    else:
+        print("无法建立数据库连接。")
